205ede344ac95fad174cd04def1de0347998c1fe
Jaan Janesmae <jaan@janesmae.com>
2015-11-15 07:35:03 +0200

    fixed untracked files and install script

--- install.sh
+++ install.sh
@@ -1,9 +1,13 @@
 # Starting with symlinks
-ln -sfv "~/.dotfiles/system/.aliases" ~
-ln -sfv "~/.dotfiles/system/.zshrc" ~
-ln -sfv "~/.dotfiles/system/.vimrc" ~
-ln -sfv "~/.dotfiles/git/.gitconfig" ~
-ln -sfv "~/.dotfiles/git/.gitignore_global" ~
+ln -sfv ~/.dotfiles/system/.aliases ~
+ln -sfv ~/.dotfiles/system/.zshrc ~
+ln -sfv ~/.dotfiles/system/.vimrc ~
+
+# Git symlinks and default config
+ln -sfv ~/.dotfiles/git/.gitconfig ~
+ln -sfv ~/.dotfiles/git/.gitignore_global ~
+git config --global core.excludesfile ~/.gitignore_global
 
 # Finally, set zsh as default shell
 chsh -s $(which zsh)
+
